fcbd4ffe7d65234dcaa40d534c09f050adfb799f
Ezequiel Balcaldi <ezequielbalcaldi@hotmail.com>
2018-09-18 19:58:16 -0300

    Agrego el route

---
+++ route.php
@@ -0,0 +1,18 @@
+<?php
+/*
+if($_GET['action'] == ''){
+  //loadHome();
+}
+else{
+  $partesURL = explode ("/", $_GET['action']);
+  if($partesURL[0] === 'tabla'){
+  //  loadTabla();
+  }
+  elseif($partesURL[0] === 'lista'){
+  //  loadLista();
+  }
+  elseif($partesURL[0] === 'registro'){
+  //  loadRegistro();
+  }
+} ?>
+*/
